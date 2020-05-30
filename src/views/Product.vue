6489ed6f18c19bec010de16e86b938b8f2d796e0
unknown <dhenarra18@gmail.com>
2020-05-31 00:50:32 +0700

    Connecting data Product from API

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -10,11 +10,11 @@
         <div class="row">
           <div class="col-lg-12 text-left">
             <div class="row">
-              <div class="col-lg-6">
+              <div class="col-lg-6" >
                 <div class="product-pic-zoom">
                   <img class="product-big-img" :src="defaultImg" alt />
                 </div>
-                <div class="product-thumbs">
+                <div class="product-thumbs" v-if="productDetails.galleries.length > 0">
                   <carousel
                     class="product-thumbs-track ps-slider"
                     :items="3"
@@ -22,35 +22,12 @@
                     :dots="false"
                   >
                     <div
+                    v-for="thumb in productDetails.galleries" :key="thumb.id"
                       class="pt"
-                      @click="changeImage(dataThumb[0])"
-                      :class="dataThumb[0] == defaultImg ? 'active' :''"
+                      @click="changeImage(thumb.photo)"
+                      :class="thumb.photo == defaultImg ? 'active' :''"
                     >
-                      <img src="img/good-4.png" alt />
-                    </div>
-
-                    <div
-                      class="pt"
-                      @click="changeImage(dataThumb[1])"
-                      :class="dataThumb[1] == defaultImg ? 'active' :''"
-                    >
-                      <img src="img/good-5.png" alt />
-                    </div>
-
-                    <div
-                      class="pt"
-                      @click="changeImage(dataThumb[2])"
-                      :class="dataThumb[2] == defaultImg ? 'active' :''"
-                    >
-                      <img src="img/good-6.png" alt />
-                    </div>
-
-                    <div
-                      class="pt"
-                      @click="changeImage(dataThumb[3])"
-                      :class="dataThumb[3] == defaultImg ? 'active' :''"
-                    >
-                      <img src="img/good-7.png" alt />
+                      <img :src="thumb.photo" alt />
                     </div>
                   </carousel>
                 </div>
@@ -58,18 +35,18 @@
               <div class="col-lg-6">
                 <div class="product-details">
                   <div class="pd-title">
-                    <span>oranges</span>
-                    <h3>Pure Pineapple</h3>
+                    <span>{{ productDetails.type }}</span>
+                    <h3>{{ productDetails.name }}</h3>
                   </div>
                   <div class="pd-desc">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.</p>
+                    <p>{{ productDetails.description }}</p>
                     <p>
                       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus quisquam animi, commodi, nihil voluptate nostrum neque architecto illo officiis doloremque et corrupti cupiditate voluptatibus error illum. Commodi expedita animi nulla aspernatur.
                       Id asperiores blanditiis, omnis repudiandae iste inventore cum, quam sint molestiae accusamus voluptates ex tempora illum sit perspiciatis. Nostrum dolor tenetur amet, illo natus magni veniam quia sit nihil dolores.
                       Commodi ratione distinctio harum voluptatum velit facilis voluptas animi non laudantium, id dolorem atque perferendis enim ducimus? A exercitationem recusandae aliquam quod. Itaque inventore obcaecati, unde quam
                       impedit praesentium veritatis quis beatae ea atque perferendis voluptates velit architecto?
                     </p>
-                    <h4>$495.00</h4>
+                    <h4>${{ productDetails.price }}</h4>
                   </div>
                   <div class="quantity">
                     <router-link to="/cart">
@@ -95,7 +72,7 @@
 import HeaderHome from "../components/HeaderHome";
 import BreadCrumb from "../components/BreadCrumb";
 import RelatedProduct from "../components/RelatedProduct";
-// import TeamMember from "../components/TeamMember";
+import axios from "axios";
 // import PartnerTeam from "../components/PartnerTeam";
 import Footer from "../components/Footer";
 import carousel from "vue-owl-carousel";
@@ -113,19 +90,39 @@ export default {
   },
   data() {
     return {
-      defaultImg: "img/good-4.png",
+      defaultImg: "",
       dataThumb: [
         "img/good-4.png",
         "img/good-5.png",
         "img/good-6.png",
         "img/good-7.png"
-      ]
-    };
+      ],
+      productDetails: []
+
+      // check product id
+      // productId: this.$route.params.id
+    }
   },
   methods: {
     changeImage(urlImage) {
       this.defaultImg = urlImage;
+      // check product id
+      // console.log(this.productDetails);
+    },
+    getProductDataAndPicture(data){
+      this.productDetails = data;
+      this.defaultImg = data.galleries[0].photo;
     }
+  },
+  mounted() {
+    axios
+      .get("http://shayna-backend.belajarkoding.com/api/products/", {
+        params: {
+          id: this.$route.params.id
+        }
+      })
+      .then(res => (this.getProductDataAndPicture(res.data.data)))
+      .catch(err => console.log(err));
   }
 };
 </script>
